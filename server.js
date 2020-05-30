e6f4e93142896a54c06f26f4e9ff1b913ca7fc37
Maheshkumar Palled <maheshmechx@gmail.com>
2020-05-30 12:09:15 -0700

    Week-1 Assignment

--- server.js
+++ server.js
@@ -1,67 +1,33 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const campsiteRouter = require("./routes/campsiteRouter");
+const promotionRouter = require("./routes/promotionRouter");
+const partnerRouter = require("./routes/partnerRouter");
 
-
-const hostname = 'localhost';
+const hostname = "localhost";
 const port = 3000;
-
 const app = express();
-app.use(morgan('dev'));
 
-app.use(express.static(__dirname + '/public'));
+app.use(morgan("dev"));
 
 app.use(bodyParser.json());
 
-app.all('/campsites', (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-});
+app.use("/campsites", campsiteRouter);
+app.use("/promotions", promotionRouter);
+app.use("/partners", partnerRouter);
 
-app.get('/campsites', (req, res) => {
-    res.end('Will send all the campsites to you');
-});
+app.use(express.static(__dirname + "/public"));
 
-app.post('/campsites', (req, res) => {
-    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
-});
-
-app.put('/campsites', (req, res) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /campsites');
-});
-
-app.delete('/campsites', (req, res) => {
-    res.end('Deleting all campsites');
-});
-
-app.get('/campsites/:campsiteId', (req, res) => {
-    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
-});
-
-app.post('/campsites/:campsiteId', (req, res) => {
-    res.statusCode = 403;
-    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
-});
-
-app.put('/campsites/:campsiteId', (req, res) => {
-    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-    res.end(`Will update the campsite: ${req.body.name}
-        with description: ${req.body.description}`);
-});
-
-app.delete('/campsites/:campsiteId', (req, res) => {
-    res.end(`Deleting campsite: ${req.params.campsiteId}`);
-});
+app.use(bodyParser.json());
 
 app.use((req, res) => {
-    console.log(req.headers);
-    res.statusCode=200;
-    res.setHeader('Content-type','text/html');
-    res.end('<html><body><h1>This is the express server..! </h1></body></html>');
+  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader("Content-type", "text/html");
+  res.end("<html><body><h1>This is the express server..! </h1></body></html>");
 });
 
-app.listen(port, hostname, ()=>{
-    console.log(`Server is runnig at http://${hostname}:${port}`);
-})
+app.listen(port, hostname, () => {
+  console.log(`Server is runnig at http://${hostname}:${port}`);
+});
